f43b52fc9e7b04c18f8d6ec6c11fd7c59357de87
BigBossYolanda <st10507150@rcconnect.edu.za>
2026-04-13 22:35:25 +0200

    Update Login.java

--- src/Login.java
+++ src/Login.java
@@ -5,17 +5,20 @@ public class Login {
   private String firstName;
   private String lastName;
 
+//Constructor  
   public Login(String username, String password, String firstName, String lastName){
       this.username = username;
       this.password = password;
       this.firstName = firstName;
       this.lastName = lastName;
   }
+
+//method to attempt login  
   public String LoginUser(String enteredUsername, String enteredPassword){
       if (enteredUsername.equals(this.username)&&enteredPassword.equals(this.password)){
           return "Welcome" + "," + firstName + " " + lastName + "," + "It is great to see you again.";
       }else {
-          return "Username or password incorrect. Please again.";
+          return "Username or password incorrect. Please try again.";
       }
   }
 }
